2dcd0cf7978a0490b6b5e75a954a76176334f912
Alex <info@jarroo.com>
2016-02-06 23:27:14 +0100

    Init

---
+++ Pago/Pago.h
@@ -0,0 +1,19 @@
+//
+//  Pago.h
+//  Pago
+//
+//  Created by Alex Manarpies on 06/02/16.
+//  Copyright © 2016 Jarroo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pago.
+FOUNDATION_EXPORT double PagoVersionNumber;
+
+//! Project version string for Pago.
+FOUNDATION_EXPORT const unsigned char PagoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pago/PublicHeader.h>
+
+
